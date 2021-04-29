7321250a49952bd9544bbbaa6012d049dd83dbf6
Hrishikesh Malge <hrishikeshmalge@gmail.com>
2021-04-29 14:41:08 +0530

    First commit

---
+++ src/demo/addition.java
@@ -0,0 +1,12 @@
+package demo;
+
+public class addition {
+	
+	public static void main(String[] args) {
+		
+		int a=17500, b=17500, vijaySir=0;
+		vijaySir = a+b;
+		System.out.println(vijaySir+" im very happy to gave me excellent knowledge");
+	}
+
+}
